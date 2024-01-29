601ecc8b9915a60d939017448e5ab60435c18be3
RikuLehtonen97 <47364@edu.raseko.fi>
2024-01-29 15:19:22 +0200

    shhh

--- kysely/admin.py
+++ kysely/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Kysymys, Vaihtoehto
 
+admin.site.register(Kysymys)
 @admin.register(Vaihtoehto)
+
 class VaihtoehtoAdmin(admin.ModelAdmin):
     list_display = ["kysymys", "teksti"]
