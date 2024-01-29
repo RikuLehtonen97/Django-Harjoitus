b103442d4da1b6ecc2799b61ffd02412f0f466b6
RikuLehtonen97 <47364@edu.raseko.fi>
2024-01-29 13:35:54 +0200

    Lisää kysely-appiin etusivu

--- kysely/views.py
+++ kysely/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def indeksi(request):
+    return HttpResponse("Hyppää voltti! olet kysely-apin index-sivulla!.")
+
