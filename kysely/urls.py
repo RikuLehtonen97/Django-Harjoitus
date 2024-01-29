2f0796846e7cb9dc32a7e018d888c1c6095186b4
RikuLehtonen97 <47364@edu.raseko.fi>
2024-01-29 15:56:09 +0200

    feeze more

--- kysely/urls.py
+++ kysely/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
 from . import views
+
 urlpatterns = [
-    path("", views.indeksi, name="index"),
+    # ex: /kyselyt/
+     path("", views.indeksi, name="index"),
+    # ex: /kyselyt/5/
+    path("<int:question_id>/", views.yksityiskohdat, name="yksityiskohdat"),
+    # ex: /kyselyt/5/tulokset/
+    path("<int:question_id>/results/", views.tulokset, name="tulokset"),
+    # ex: /kyselyt/5/äänestä/
+    path("<int:question_id>/vote/", views.äänestä, name="äänestä"),
 ]
