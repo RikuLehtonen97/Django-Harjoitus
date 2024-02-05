31154f71815f623be04ddb03f41117e5302770dc
RikuLehtonen97 <47364@edu.raseko.fi>
2024-02-05 15:28:31 +0200

    v2

--- kysely/urls.py
+++ kysely/urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 
 from . import views
 
-
-app_name = "kysely<"
+app_name = "kysely"
 urlpatterns = [
     # esim. /kyselyt/
     path("", views.indeksi, name="indeksi"),
     # esim. /kyselyt/5/
     path("<int:kysymys_id>/", views.näytä, name="näytä"),
     # esim. /kyselyt/5/results/
-    path("<int:question_id>/tulokset/", views.tulokset, name="tulokset"),
+    path("<int:kysymys_id>/tulokset/", views.tulokset, name="tulokset"),
     # esim. /kyselyt/5/äänestä/
-    path("<int:question_id>/äänestä/", views.äänestä, name="äänestä"),
+    path("<int:kysymys_id>/äänestä/", views.äänestä, name="äänestä"),
 ]
