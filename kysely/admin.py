05bd4c9008f5a677d67528875552069947447a46
RikuLehtonen97 <47364@edu.raseko.fi>
2024-02-12 16:10:41 +0200

    12/02/2024 16:11

--- kysely/admin.py
+++ kysely/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 
 from .models import Kysymys, Vaihtoehto
 
-admin.site.register(Kysymys)
+
+class VastausvaihtoehtoInline(admin.TabularInline):
+    model = Vaihtoehto
+    extra = 3
+
+
+@admin.register(Kysymys)
+class KysymysAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Päivämäärätiedot", {"fields": ["julkaisupvm"]}),
+        ("Sisältö", {"fields": ["teksti"]}),
+    ]
+    inlines = [VastausvaihtoehtoInline]
+    list_display = ["teksti", "julkaisupvm", "onko_julkaistu_lähiaikoina"]
+
 
 @admin.register(Vaihtoehto)
 class VaihtoehtoAdmin(admin.ModelAdmin):
